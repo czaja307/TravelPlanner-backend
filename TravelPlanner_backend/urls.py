492259d530885cca7d58404368aee6b4fc56dd83
Jakub Czajkowski <272709@student.pwr.edu.pl>
2024-06-14 02:32:33 +0200

    add swagger to default url

--- TravelPlanner_backend/urls.py
+++ TravelPlanner_backend/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 from drf_spectacular.views import SpectacularSwaggerView, SpectacularRedocView, SpectacularAPIView
 
 urlpatterns = [
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
