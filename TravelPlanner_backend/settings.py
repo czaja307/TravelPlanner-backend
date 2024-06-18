d0a8cce2ffe0157a41a7de3c9a1bf07fdc196d7b
Jakub Czajkowski <272709@student.pwr.edu.pl>
2024-06-18 18:48:19 +0200

    remove debug prints

--- TravelPlanner_backend/settings.py
+++ TravelPlanner_backend/settings.py
@@ -65,7 +65,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     # TODO: change to 5 minutes
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
