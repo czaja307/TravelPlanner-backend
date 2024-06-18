5343e9362cbde168ef582d4efebf55c8a4cc9123
Jakub Czajkowski <272709@student.pwr.edu.pl>
2024-06-19 00:15:19 +0200

    update access token lifetime

--- TravelPlanner_backend/settings.py
+++ TravelPlanner_backend/settings.py
@@ -64,8 +64,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    # TODO: change to 5 minutes
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
