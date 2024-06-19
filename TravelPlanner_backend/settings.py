76396e12a1fc4742aabc3817623133f70b081c6c
Szymon Kowaliński <szymon@kowalinski.dev>
2024-06-19 02:19:20 +0200

    fix: uncomment db settings

--- TravelPlanner_backend/settings.py
+++ TravelPlanner_backend/settings.py
@@ -146,8 +146,8 @@ DATABASES = {
         "NAME": os.environ.get("DB_NAME", BASE_DIR / "db.sqlite3"),
         "USER": os.environ.get("DB_USER", "user"),
         "PASSWORD": os.environ.get("DB_PASSWORD", "password"),
-        # "HOST": os.environ.get("DB_HOST", "localhost"),
-        # "PORT": os.environ.get("DB_PORT", "5432"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 
